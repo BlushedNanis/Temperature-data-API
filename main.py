66a250303ad566ac8876d1e196d4ee353ecf0ca8
BlushedNanis <luis_danieltg@outlook.com>
2024-03-27 02:23:13 -0400

    Catch errors from non-valid stations and dates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
-
+from numpy import float64
 
 app = Flask(__name__)
 
@@ -12,15 +12,19 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    filepath = "data\TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
-    return {"station": station,
-            "date": date,
-            "temperature": temperature
-        
-    }
-
-
+    try:
+        filepath = "data\TG_STAID" + str(station).zfill(6) + ".txt"
+        df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+        temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+        print(type(temperature))
+        if type(temperature) is float64:
+            return {"station": station,
+                    "date": date,
+                    "temperature": temperature}
+        else:
+            return "Please, provide a valid date."
+    except FileNotFoundError:
+        return "Please, provide a valid station."
+    
 if __name__ == "__main__":
     app.run(debug=True)
