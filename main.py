ad2c780b65ce1f6b96a81b565bfcfbcf489f3f52
BlushedNanis <luis_danieltg@outlook.com>
2024-03-28 00:49:19 -0400

    Added table of stations to home page

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from numpy import float64
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data\stations.txt", skiprows=17)
+stations = stations[["STAID","STANAME                                 "]]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", stations_id=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -16,7 +19,6 @@ def api(station, date):
         filepath = "data\TG_STAID" + str(station).zfill(6) + ".txt"
         df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
         temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
-        print(type(temperature))
         if type(temperature) is float64:
             return {"station": station,
                     "date": date,
