44625c9774ad487d5a672066f00ee66e3602833e
BlushedNanis <luis_danieltg@outlook.com>
2024-03-28 02:16:35 -0400

    Added endpoint to show all data from a station

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
-from numpy import float64
+from numpy import float64, nan
 
 app = Flask(__name__)
 
@@ -13,8 +13,18 @@ def home():
     return render_template("home.html", stations_id=stations.to_html())
 
 
+@app.route("/api/v1/<station>")
+def only_station(station):
+    filepath = "data\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+    df["TR"] = df["   TG"].mask(df["   TG"] == -9999, nan)
+    df["TC"] = df["TR"] / 10
+    df["TF"] = round((df["TC"] * 9/5 + 32), 2)
+    result_df = df[["    DATE", "TC", "TF"]].to_dict('records')
+    return result_df
+
 @app.route("/api/v1/<station>/<date>")
-def api(station, date):
+def station_and_date(station, date):
     try:
         filepath = "data\TG_STAID" + str(station).zfill(6) + ".txt"
         df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
@@ -27,6 +37,7 @@ def api(station, date):
             return "Please, provide a valid date."
     except FileNotFoundError:
         return "Please, provide a valid station."
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
