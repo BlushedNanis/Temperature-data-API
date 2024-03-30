5a12900646cfb22fb1ad246214a74820262b6379
BlushedNanis <luis_danieltg@outlook.com>
2024-03-30 00:28:19 -0400

    Added comments

--- main.py
+++ main.py
@@ -17,14 +17,22 @@ def home():
 def station_and_date(station, year, month, day):
     try:
         if len(year) == 4 and len(month) == 2 and len(day) == 2:
+            #Construct date variable with endpoint data
             date = year + "-" + month + "-" + day
+            
+            #Construct filepath variable with endpoint data
             filepath = "data\\TG_STAID" + str(station).zfill(6) + ".txt"
+            
+            #Create data frame instance and get requiered row
             df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
             day_df = df.loc[df["    DATE"] == date][["    DATE", "   TG"]]
+            
             #Add columns for celcius and fahrenheit temperatures
             day_df["TR"] = day_df["   TG"].mask(df["   TG"] == -9999, nan)
             day_df["TC"] = day_df["TR"] / 10
             day_df["TF"] = round((day_df["TC"] * 9/5 + 32), 2)
+            
+            #Construct result data frame instance
             result_df = day_df[["    DATE", "TC", "TF"]].to_dict('records')
             return result_df
         else:
@@ -37,16 +45,26 @@ def station_and_date(station, year, month, day):
 def station_and_month(station, year, month):
     try:
         if len(year) == 4 and len(month) == 2:
+            #Construct date variable with endpoint data
             date = year + month
+            
+            #Construct filepath variable with endpoint data
             filepath = "data\\TG_STAID" + str(station).zfill(6) + ".txt"
+            
+            #Create data frame instance from filepath
             df = pd.read_csv(filepath, skiprows=20)
+            
+            #Get requiered rows according to date
             df["    DATE"] = df["    DATE"].astype(str)
             year_df = df[df["    DATE"].str.startswith(date)]
             year_df["    DATE"] = pd.to_datetime(year_df["    DATE"])
+            
             #Add columns for celcius and fahrenheit temperatures
             year_df["TR"] = year_df["   TG"].mask(df["   TG"] == -9999, nan)
             year_df["TC"] = year_df["TR"] / 10
             year_df["TF"] = round((year_df["TC"] * 9/5 + 32), 2)
+            
+            #Construct result data frame instance
             result_df = year_df[["    DATE", "TC", "TF"]].to_dict('records')
             return result_df
         else:
@@ -59,16 +77,24 @@ def station_and_month(station, year, month):
 def station_and_year(station, year):
     try:
         if len(year) <= 4:
+            #Construct date variable with endpoint data
             date = year
+            
+            #Construct filepath variable with endpoint data
             filepath = "data\\TG_STAID" + str(station).zfill(6) + ".txt"
+            
+            #Get requiered rows according to date
             df = pd.read_csv(filepath, skiprows=20)
             df["    DATE"] = df["    DATE"].astype(str)
             year_df = df[df["    DATE"].str.startswith(date)]
             year_df["    DATE"] = pd.to_datetime(year_df["    DATE"])
+            
             #Add columns for celcius and fahrenheit temperatures
             year_df["TR"] = year_df["   TG"].mask(df["   TG"] == -9999, nan)
             year_df["TC"] = year_df["TR"] / 10
             year_df["TF"] = round((year_df["TC"] * 9/5 + 32), 2)
+            
+            #Construct result data frame instance
             result_df = year_df[["    DATE", "TC", "TF"]].to_dict('records')
             return result_df
         else:
@@ -79,12 +105,18 @@ def station_and_year(station, year):
 @app.route("/api/v1/<station>")
 def only_station(station):
     try:
+        #Construct filepath variable with endpoint data
         filepath = "data\\TG_STAID" + str(station).zfill(6) + ".txt"
+        
+        #Create data frame instance from filepath
         df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+        
         #Add columns for celcius and fahrenheit temperatures
         df["TR"] = df["   TG"].mask(df["   TG"] == -9999, nan)
         df["TC"] = df["TR"] / 10
         df["TF"] = round((df["TC"] * 9/5 + 32), 2)
+        
+        #Construct result data frame instance
         result_df = df[["    DATE", "TC", "TF"]].to_dict('records')
         return result_df
     except FileNotFoundError:
